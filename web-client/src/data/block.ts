cad6d68770a53ed2ef90f8c3cb2940c8de6f62ea
bshawn <bret.shawn@gmail.com>
2019-10-03 22:13:30 -0400

    add Block.fromBlockResult()

--- web-client/src/data/block.ts
+++ web-client/src/data/block.ts
@@ -1,25 +1,14 @@
+import { GetBlockResult } from "eosjs/dist/eosjs-rpc-interfaces";
+
 export class Block {
-  private _id: string = "";
-  public get id(): string {
-    return this._id;
-  }
-  public set id(v: string) {
-    this._id = v;
-  }
+  public id: string = "";
+  public timestamp: string = "";
+  public actionCount: number = 0;
 
-  private _timestamp: string = "";
-  public get timestamp(): string {
-    return this._timestamp;
-  }
-  public set timestamp(v: string) {
-    this._timestamp = v;
-  }
-
-  private _actionCount: number = 0;
-  public get actionCount(): number {
-    return this._actionCount;
-  }
-  public set actionCount(v: number) {
-    this._actionCount = v;
+  public static fromBlockResult(dto: GetBlockResult): Block {
+    const block = new Block();
+    block.id = dto.id;
+    block.timestamp = dto.timestamp;
+    return block;
   }
 }
