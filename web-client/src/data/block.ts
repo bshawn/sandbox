7c9f5edac6f4d0c94108903b931bef735a34bcf9
bshawn <bret.shawn@gmail.com>
2019-10-08 22:23:06 -0400

    calculate action count

--- web-client/src/data/block.ts
+++ web-client/src/data/block.ts
@@ -1,5 +1,9 @@
 import { GetBlockResult } from "eosjs/dist/eosjs-rpc-interfaces";
 
+interface GetBlockResultWithTransactions extends GetBlockResult {
+  transactions?: any[];
+}
+
 export class Block {
   public id: string = "";
   public timestamp: string = "";
@@ -7,7 +11,9 @@ export class Block {
   public actionCount: number = 0;
   public raw: string = "";
 
-  public static fromBlockResult(blockResult: GetBlockResult): Block {
+  public static fromBlockResult(
+    blockResult: GetBlockResultWithTransactions
+  ): Block {
     if (!blockResult) throw new Error("blockResult was null or undefined");
 
     const block = new Block();
@@ -15,6 +21,21 @@ export class Block {
     block.timestamp = blockResult.timestamp;
     block.previousId = blockResult.previous;
     block.raw = JSON.stringify(blockResult, null, 2);
+    block.actionCount = this.calculateActions(blockResult);
     return block;
+  }
+
+  private static calculateActions(
+    blockResult: GetBlockResultWithTransactions
+  ): number {
+    if (!blockResult.transactions) return 0;
+
+    return blockResult.transactions.reduce<number>((prev, current) => {
+      if (current.trx instanceof Object) {
+        return current.trx.transaction.actions.length + prev;
+      }
+
+      return 0;
+    }, 0);
   }
 }
