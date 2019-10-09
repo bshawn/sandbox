aeb6ff2825c37cbe4950d092c7a913f6384bdee3
bshawn <bret.shawn@gmail.com>
2019-10-08 20:37:50 -0400

    tweak styling of block elements

--- web-client/src/block-element.tsx
+++ web-client/src/block-element.tsx
@@ -9,29 +9,38 @@ interface BlockElementProps {
 
 const BlockElement: React.FC<BlockElementProps> = props => {
   return (
-    <div className="box" onClick={props.onClick}>
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-            <img src="./blockchain-128.png" alt="Block Chain Icon" />
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>{props.block.id}</strong>
-            </p>
-            <p>
-              <small>{props.block.timestamp}</small>
-            </p>
-            <p>
-              <pre>
-                <code>{props.block.raw}</code>
-              </pre>
-            </p>
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img src="./blockchain-128.png" alt="Block Chain Icon" />
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{props.block.id}</strong>
+                </p>
+                <p>
+                  <small>{props.block.timestamp}</small>
+                </p>
+              </div>
+            </div>
           </div>
+        </p>
+        <a href="#" className="card-header-icon" aria-label="more options">
+          <span className="icon">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          <pre>{props.block.raw}</pre>
         </div>
-      </article>
+      </div>
     </div>
   );
 };
